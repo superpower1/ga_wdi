6ae3d61a3d58517f6bbceb829b6688c3512fb0ed
Haofu <haofu.zhu@gmail.com>
2017-12-13 13:35:47 +1100

    finish basic functions

--- project1/app.js
+++ project1/app.js
@@ -1,50 +1,87 @@
 const tableDiv = document.querySelector('.table');
+const resultDiv = document.querySelector('.result');
+const winnerDiv = document.querySelector('.winner');
 const table = [];
 
-let switchPlayer = false;
+let switchPlayer;
 
 const isAllOjectContentSame = (arr) => {
-  const temp = arr[0];
-  if (temp==="") {
+  const temp = arr[0].textContent;
+  if (!temp) {
     return false;
   }
   return arr.every(ele => {
-    return ele.textContent === temp.textContent;
+    return ele.textContent === temp;
   });
 }
 
-const checkForWin = (table) => {
-  let flag = false;
+const checkTable = (table) => {
+  let winner = "";
   table.forEach(row => {
     if (isAllOjectContentSame(row)) {
-      flag = true;
+      winner = row[0].textContent;
     }
   });
+  return winner;
+}
+
+const checkForWin = (table) => {
+
+  let winner;
+
+  if (checkTable(table)) {
+    winner = checkTable(table);
+  }
 
   //transpose the matrix
   const transposedTable = table[0].map((col, i) => table.map(row => row[i]));
 
-  transposedTable.forEach(row => {
-    if (isAllOjectContentSame(row)) {
-      flag = true;
-    }
-  });
+  if (checkTable(transposedTable)) {
+    winner = checkTable(transposedTable);
+  }
 
   const diagonal = [[table[0][0], table[1][1], table[2][2]], [table[2][0], table[1][1], table[0][2]]];
 
-  diagonal.forEach(row => {
-    if (isAllOjectContentSame(row)) {
-      flag = true;
-    }
-  });
+  if (checkTable(diagonal)) {
+    winner = checkTable(diagonal);
+  }
 
+  return winner;
+
+}
+
+const isAllFilled = (table) => {
+  let flag = true;
+  table.forEach(row => {
+    row.forEach(ele => {
+      if (!ele.textContent) {
+        flag = false;
+      }
+    });
+  });
   return flag;
+}
+
+const endOfGame = () => {
+  const winner = checkForWin(table);
+
+  if (winner) {
+    winnerDiv.textContent = `Winner is ${winner}`;
+    resultDiv.classList.remove('hidden');
+
+    console.log(winner);
+  } else if (isAllFilled(table)) {
+    winnerDiv.textContent = "Draw Game";
+    resultDiv.classList.remove('hidden');
+  } else {
+
+  }
 
 }
 
 const init = () => {
   const size = 3;
-
+  switchPlayer = false;
   for (let i = 0; i < size; i++) {
     const row = [];
     for (let j = 0; j < size; j++) {
@@ -63,7 +100,7 @@ const init = () => {
           switchPlayer = !switchPlayer;
         }
         console.log(`${e.target.textContent}`);
-        console.log(checkForWin(table));
+        endOfGame();
       });
       tableDiv.appendChild(newBlock);
     }
@@ -71,6 +108,17 @@ const init = () => {
   }
 }
 
+const reset = () => {
+  switchPlayer = false;
+  table.forEach(row => {
+    row.forEach(item => {
+      item.textContent = "";
+    });
+  });
+  document.querySelector('.result').classList.add('hidden');
+}
+
+document.querySelector('.reset button').addEventListener('click', reset);
 
 
 init();
